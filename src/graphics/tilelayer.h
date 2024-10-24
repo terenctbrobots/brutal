a39f62204b8b19edb80edebb6eab332412a7947d
Terence Tan <terence.tan@bigbadrobots.com>
2024-10-24 18:34:38 +0800

    add tile classes

---
+++ src/graphics/tilelayer.h
@@ -0,0 +1,12 @@
+#ifndef TILELAYER_H
+#define TILELAYER_H
+
+#include "layer.h"
+
+class TileLayer : public Layer 
+{
+
+};
+
+
+#endif
---
+++ src/graphics/tileset.h
@@ -0,0 +1,4 @@
+#ifndef TILEMAP_H
+#define TILEMAP_H
+
+#endif
