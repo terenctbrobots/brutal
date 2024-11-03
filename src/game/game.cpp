b3cdb4a496c8dd193669f44a0a708a93d5a37a14
Terence Tan <terence.tan@bigbadrobots.com>
2024-11-03 14:42:20 +0800

    Added clang-format and spdlogger

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,19 +1,16 @@
 #include "game.h"
 
-void Game::CreateLevel()
-{
-    level = std::make_unique<Level>();
-}
+#include "spdlog/spdlog.h"
 
-Game& Game::Get()
-{
+void Game::CreateLevel() { level = std::make_unique<Level>(); }
+
+Game &Game::Get() {
     static Game instance;
 
     return instance;
 }
 
-void Game::Setup(float width, float height)
-{
+void Game::Setup(float width, float height) {
     level = nullptr;
     view_screen.x = 0;
     view_screen.y = 0;
@@ -21,20 +18,23 @@ void Game::Setup(float width, float height)
     view_screen.height = height;
 
     InitWindow(width, height, "Game");
+#ifdef DEBUG
+    spdlog::info("Game : Setup with width {} and height {}", width, height);
+#endif
 }
 
-int Game::MainLoop()
-{
-    if (level == nullptr)
-        return 0;
+int Game::MainLoop() {
+    if (level == nullptr) return 0;
 
     level->MainLoop();
 
     return 0;
 }
 
-void Game::Cleanup()
-{
+void Game::Cleanup() {
     level = nullptr;
     CloseWindow();
+#ifdef DEBUG
+    spdlog::info("Game : Cleanup");
+#endif
 }
--- src/game/gameobject.cpp
+++ src/game/gameobject.cpp
@@ -1,27 +1,20 @@
-#include <iostream>
 #include "gameobject.h"
+
+#include <iostream>
+
 #include "game/game.h"
 #include "graphics/sprite.h"
 #include "helper.h"
 
-GameObject::GameObject(std::string const& name)
-{
-    uid_ = GetNextUid();
-}
+GameObject::GameObject(std::string const& name) { uid_ = GetNextUid(); }
 
-GameObject::~GameObject()
-{
+GameObject::~GameObject() {}
 
-}
-
-
-int GameObject::LoadSprite(std::string const& file_name)
-{
+int GameObject::LoadSprite(std::string const& file_name) {
     auto sprite = std::make_shared<Sprite>();
     int return_value = sprite->Load(file_name);
 
-    if (return_value == Graphics::OK)
-    {
+    if (return_value == Graphics::OK) {
         graphics_ = sprite;
 
         position.width = sprite->width;
@@ -31,16 +24,15 @@ int GameObject::LoadSprite(std::string const& file_name)
     return return_value;
 }
 
-void GameObject::Draw()
-{
-    if (graphics_ == nullptr)
-    {
+void GameObject::Draw() {
+    if (graphics_ == nullptr) {
         return;
     }
 
     Game& game = Game::Get();
 
-    Vector2 screen_position = {position.x - game.view_screen.x, position.y - game.view_screen.y};
+    Vector2 screen_position = {position.x - game.view_screen.x,
+                               position.y - game.view_screen.y};
 
     graphics_->Draw(screen_position);
 }
