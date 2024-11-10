56951417a9d6c98d4c3c944167352bf1fd7acd7a
Terence Tan <terence.tan@bigbadrobots.com>
2024-11-11 06:23:28 +0800

    fix ui test

--- test/ui_test.cpp
+++ test/ui_test.cpp
@@ -1,43 +1,47 @@
-#include "graphic_test.h"
-#include "raylib.h"
-#define RAYGUI_IMPLEMENTATION
 #include <fstream>
 
 #include "game/gameobject.h"
 #include "game/level.h"
-#include "raygui.h"
+#include "graphic_test.h"
+#include "raylib.h"
 #include "ui/button.h"
 #include "ui/text.h"
 
 using namespace Brutal;
 
 TEST_F(GraphicTest, TestUI) {
-    // Level level = Level();
+    Level level = Level();
 
-    // auto gameobject = level.CreateGameObject("test");
+    auto gameobject = level.CreateGameObject("test");
 
-    // std::ifstream text_json("testdata/text.json");
+    std::ifstream text_json("testdata/text.json");
 
-    // json parsed = json::parse(text_json);
+    json text_parsed = json::parse(text_json);
 
-    // auto& text = gameobject.AddComponent<TextComponent>(Text::Deserialize(parsed));
-    // int frame_counter = 0;
-    // bool exit_flag = false;
-    // SetTargetFPS(60);
-    // Vector2 position = {100, 100};
+    auto& text = gameobject.AddComponent<TextComponent>(Text::Deserialize(text_parsed));
 
-    // while (!WindowShouldClose() && !exit_flag) {
-    //     frame_counter++;
+    std::ifstream button_json("testdata/button.json");
+    json button_parsed = json::parse(button_json);
 
-    //     BeginDrawing();
-    //     ClearBackground(BLACK);
+    auto& button = gameobject.AddComponent<ButtonComponent>(Button::Deserialize(button_parsed));
+    int frame_counter = 0;
+    bool exit_flag = false;
+    SetTargetFPS(60);
+    Vector2 position = {100, 100};
 
-    //     Text::Draw(position, text);
+    while (!WindowShouldClose() && !exit_flag) {
+        frame_counter++;
 
-    //     EndDrawing();
+        BeginDrawing();
+        ClearBackground(BLACK);
 
-    //     if (frame_counter >= this->delay_frames) {
-    //         exit_flag = true;
-    //     }
-    // }
+        Text::Draw(position, text);
+        Button::Draw({100, 200}, button);
+
+        EndDrawing();
+
+        if (frame_counter >= this->delay_frames) {
+            exit_flag = true;
+        }
+    }
 }
