ed1b2a2a2074be105574855610466e5ad7b952b7
Terence Tan <terence.tan@bigbadrobots.com>
2024-11-19 08:14:56 +0800

    Update clang format to use egyptian style braces

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,19 +1,23 @@
 #include "game.h"
 
-namespace Brutal {
-void Game::CreateLevel() {
+namespace Brutal
+{
+void Game::CreateLevel()
+{
     level = new Level();
     level->Setup();
     level->UpdateView({0, 0, width_, height_});
 }
 
-Game &Game::Get() {
+Game &Game::Get()
+{
     static Game instance;
 
     return instance;
 }
 
-void Game::Setup(float width, float height) {
+void Game::Setup(float width, float height)
+{
     level = nullptr;
 
     width_ = width;
@@ -25,20 +29,24 @@ void Game::Setup(float width, float height) {
 #endif
 }
 
-int Game::MainLoop() {
-    if (level == nullptr) return 0;
+int Game::MainLoop()
+{
+    if (level == nullptr)
+        return 0;
 
     level->MainLoop();
 
     return 0;
 }
 
-void Game::Cleanup() {
+void Game::Cleanup()
+{
 #ifdef DEBUG
     spdlog::info("Game : Cleanup");
 #endif
 
-    if (level != NULL) {
+    if (level != NULL)
+    {
         level->Cleanup();
         delete level;
     }
@@ -46,4 +54,4 @@ void Game::Cleanup() {
     CloseWindow();
 }
 
-}  // namespace Brutal
+} // namespace Brutal
